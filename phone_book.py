a9086bdfb2b144b070323d5dcca6ffc976c70217
KateChubarava <k.chubarava@altoros.com>
2023-12-27 16:19:40 +0100

    phone book

---
+++ phone_book.py
@@ -0,0 +1,22 @@
+def process(line):
+    commands = line.split()
+    command = commands[0]
+    phone = int(commands[1])
+    if command == 'add':
+        name = commands[2]
+        arr[phone] = name
+    elif command == 'find':
+        name = arr[phone]
+        if name is not None:
+            print(name)
+        else:
+            print('not found')
+    elif command == 'del':
+        arr[phone] = None
+
+arr = [None] * 10000001
+
+n = int(input())
+
+for i in range(n):
+    process(input())
